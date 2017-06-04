95a61224dc5906bcbc74f44fc3d98a6cbea10501
janjira <janjira.eriksson@gmail.com>
2017-06-04 21:37:56 +0200

    Updated WpRest

--- WpRest/src/main/java/com/wizepass/controller/DataController.java
+++ WpRest/src/main/java/com/wizepass/controller/DataController.java
@@ -40,26 +40,26 @@ public class DataController {
 					return listOfUsers;		
 		}
 	
-	public List<Map<String,Object>> getMg(){
-		List<Map<String, Object>> listOfUsers = new ArrayList<Map<String,Object>>();
-		SearchResponse searchResponse = null;
-		int scrollsize = 1000;
-		int i = 0;
-		while ( searchResponse == null || searchResponse.getHits().hits().length !=0){
-					searchResponse = client.prepareSearch(Constants.INDEX)
-												.setTypes(Constants.TYPE_MG)
-												.setQuery(QueryBuilders.matchAllQuery())
-												.setSize(scrollsize)
-												.setFrom(i * scrollsize)
-												.execute()
-												.actionGet();
-					for(SearchHit hit : searchResponse.getHits()){
-						listOfUsers.add(hit.getSource());
-		            	}
-		            i++;
-		        }
-					return listOfUsers;		
-		}
+//	public List<Map<String,Object>> getMg(){
+//		List<Map<String, Object>> listOfUsers = new ArrayList<Map<String,Object>>();
+//		SearchResponse searchResponse = null;
+//		int scrollsize = 1000;
+//		int i = 0;
+//		while ( searchResponse == null || searchResponse.getHits().hits().length !=0){
+//					searchResponse = client.prepareSearch(Constants.INDEX)
+//												.setTypes(Constants.TYPE_MG)
+//												.setQuery(QueryBuilders.matchAllQuery())
+//												.setSize(scrollsize)
+//												.setFrom(i * scrollsize)
+//												.execute()
+//												.actionGet();
+//					for(SearchHit hit : searchResponse.getHits()){
+//						listOfUsers.add(hit.getSource());
+//		            	}
+//		            i++;
+//		        }
+//					return listOfUsers;		
+//		}
 	
 	public List<Map<String,Object>> getRegistrationToken(){
 		List<Map<String, Object>> listOfReg = new ArrayList<Map<String,Object>>();
--- WpRest/src/main/java/com/wizepass/util/Constants.java
+++ WpRest/src/main/java/com/wizepass/util/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 	
 	public static final String ES_HOST= "localhost";
 	public static final int PORT = 9300;
-	public static final String INDEX = "wpdb";
+	public static final String INDEX = "admindb";
 	public static final String 	TYPE_USERS = "users";
-	public static final String TYPE_REG = "regtokens";
-	public static final String TYPE_MG = "mg";
+	public static final String TYPE_REG = "registrationtokens";
+	//public static final String TYPE_MG = "mg";
 }
